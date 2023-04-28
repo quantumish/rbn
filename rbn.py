fc04cebc3a008d843e771468cbffaf77c940a501
quantumish <freifeld.david@gmail.com>
2023-04-27 22:01:25 -0700

    Implement rule generation, RBN updates

--- rbn.py
+++ rbn.py
@@ -1,23 +1,29 @@
 import csv
 from dataclasses import dataclass
 import warnings
-from typing import Annotated, Union
+from typing import Annotated, Union, List, NewType
 from annotated_types import Gt, MultipleOf
 import os
 import random
 import pyvis.network
+from enum import Enum
+from bitarray import bitarray
 
-def pairwise(iterable):
+def pairwise(iterable, last=None):
     it = iter(iterable)
-    a = next(it, None)
+    a = next(it, last)
     for b in it:
         yield (a, b)
         a = b
-    yield (a, None)
+    yield (a, last)    
+    
+Bit = NewType("Bit", Annotated[int, MultipleOf(1)])
+BitArray = NewType("BitArray", bitarray)
 
 @dataclass
 class Node:
-    act: Annotated[int, MultipleOf(1)]
+    rule: BitArray
+    act: Bit # TODO should really be pulled out into its own array
     label: str
     x: float
     y: float
@@ -50,6 +56,7 @@ class RBN:
                     raise NetParseError(f"Node {i} line is incomplete")
                 
                 self.nodes[i] = Node(
+                    bitarray(),
                     random.getrandbits(1),
                     vals[1].strip("\""), # HACK we're not handling quotes correctly
                     *map(float, vals[2:])
@@ -78,18 +85,47 @@ class RBN:
                     self.offsets.append(len(self.edges)-1)
                     prev = dst
 
+        for i, locs in enumerate(pairwise(self.offsets, len(self.edges))):
+            num_in = locs[1]-locs[0]            
+            bytelen = (2**num_in)//8 if num_in > 2 else 1            
+            self.nodes[i].rule.frombytes(os.urandom(bytelen))           
+
+    def async_update(self):
+        node_idx = random.randint(0, len(self.nodes))
+
+        start = self.offsets[node_idx]
+        stop = self.offsets[node_idx+1] if node_idx != len(self.offsets)-1 else len(self.edges)
+
+        inc_acts = bitarray()
+        for i in range(start, stop):
+            inc_acts.append(self.nodes[i].act)
+
+        rule_idx = int(inc_acts.to01(), base=2) # HACK HACK HACK
+        self.nodes[node_idx].act = self.nodes[node_idx].rule[rule_idx]            
+
+    def sync_update(self):
+        new_nodes = self.nodes.copy()
+        for i, locs in enumerate(pairwise(self.offsets, len(self.edges))):
+            inc_acts = bitarray()
+            for j in range(*locs):
+                inc_acts.append(self.nodes[self.edges[j]].act)
+            rule_idx = int(inc_acts.to01(), base=2) # HACK HACK HACK
+            new_nodes[i].act = self.nodes[i].rule[rule_idx]            
+        self.nodes = new_nodes
+        
     def show_pyvis(self):
         net = pyvis.network.Network()
         for i, n in enumerate(self.nodes):
             net.add_node(i, label=n.label, value=n.act)
 
-        for i, locs in enumerate(pairwise(self.offsets)):
-            idxs = range(locs[0], locs[1] if locs[1] else len(self.edges))
-            for idx in idxs:
-                net.add_edge(i, self.edges[idx])
+        for i, locs in enumerate(pairwise(self.offsets, len(self.edges))):
+            for idx in range(*locs):
+                net.add_edge(i, self.edges[idx])        
             
         net.toggle_physics(True)
         net.show("rbn.html", notebook=False)
 
 rbn = RBN("./net/01.net")
+rbn.show_pyvis()
+rbn.sync_update()
 rbn.show_pyvis()
