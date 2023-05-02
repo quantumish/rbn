078a1a38f7d491bb21099907e496c8b07412ad96
Houjun Liu <houjun@jemoka.com>
2023-05-01 23:24:17 -0700

    thresholding

--- rbn.py
+++ rbn.py
@@ -8,6 +8,11 @@ import random
 import pyvis.network
 from enum import Enum
 from bitarray import bitarray
+import matplotlib.pyplot as plt
+import seaborn as sns
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.colors import ListedColormap
+import matplotlib.animation
 
 def pairwise(iterable, last=None):
     it = iter(iterable)
@@ -16,13 +21,12 @@ def pairwise(iterable, last=None):
         yield (a, b)
         a = b
     yield (a, last)    
-    
+
 Bit = NewType("Bit", Annotated[int, MultipleOf(1)])
 BitArray = NewType("BitArray", bitarray)
 
 @dataclass
 class Node:
-    rule: Union[BitArray, Callable[[List[Bit]], Bit]]
     act: Bit # TODO should really be pulled out into its own array
     label: str
     x: float
@@ -36,18 +40,38 @@ class NetParseWarning(Warning):
     pass
 
 class RBN:
-    def __init__(
-            self,
-            path: Union[str, os.PathLike],
-            log_histories: bool = False,
-            func: Optional[Callable[List[Bit], Bit]] = None
-    ):
+    """A brain boolean network parser
+
+    Parameters
+    ----------
+    path : os.PathLike
+        The path to the .net file to parse containing a brain map.
+    threshold : float
+        The integer threshold for update; 0.8 in paper
+    epsilon : float, optional
+        How much to fluctuate increase the threshold per node
+        "it is possible to increase this value by 0.01 every row of the matrix,
+         thus having fluctuating values (which correspond to different values between
+         the nodes)."
+    log_sistories : bool, optional
+        IDFK
+    """
+
+    def __init__(self,
+                 path: Union[str, os.PathLike],
+                 threshold: float,
+                 epsilon: float = 0,
+                 log_histories: bool = False):
         """Initializes an RBN from a .net file.
 
         NOTE: Assumes there are no spaces in the quoted labels.
         NOTE: Assumes edges are sorted.
         NOTE: .net files are one-indexed, yet this function converts everything to 0-indexed
         """
+
+        self.threshold = threshold
+        self.epsilon = epsilon
+
         with open(path, "r") as f:
             num_vtxs = f.readline()
             if num_vtxs[:10] != "*Vertices ":
@@ -64,7 +88,6 @@ class RBN:
                     raise NetParseError(f"Node {i} line is incomplete")
                 
                 self.nodes[i] = Node(
-                    bitarray() if func is None else func,
                     random.getrandbits(1),
                     vals[1].strip("\""), # HACK we're not handling quotes correctly
                     *map(float, vals[2:])
@@ -93,33 +116,18 @@ class RBN:
                     self.offsets.append(len(self.edges)-1)
                     prev = dst
 
-        if func is None:
-            for i, locs in enumerate(pairwise(self.offsets, len(self.edges))):
-                num_in = locs[1]-locs[0]            
-                bytelen = (2**num_in)//8 if num_in > 2 else 1            
-                self.nodes[i].rule.frombytes(os.urandom(bytelen))           
-            
-    def async_update(self):
-        node_idx = random.randint(0, len(self.nodes))
-
-        start = self.offsets[node_idx]
-        stop = self.offsets[node_idx+1] if node_idx != len(self.offsets)-1 else len(self.edges)
-
-        inc_acts = bitarray()
-        for i in range(start, stop):
-            inc_acts.append(self.nodes[i].act)
-
-        rule_idx = int(inc_acts.to01(), base=2) # HACK HACK HACK
-        self.nodes[node_idx].act = self.nodes[node_idx].rule[rule_idx]            
+            # seed random weights for the connection edges
+            self.weights = [random.uniform(0,1) for _ in range(len(self.edges))]
 
     def sync_update(self):
         new_nodes = self.nodes.copy()
+        threshold = self.threshold
         for i, locs in enumerate(pairwise(self.offsets, len(self.edges))):
-            inc_acts = bitarray()
+            inc_acts = []
             for j in range(*locs):
-                inc_acts.append(self.nodes[self.edges[j]].act)
-            rule_idx = int(inc_acts.to01(), base=2) # HACK HACK HACK
-            new_nodes[i].act = self.nodes[i].rule[rule_idx]            
+                inc_acts.append(self.nodes[self.edges[j]].act*self.weights[self.edges[j]])
+            new_nodes[i].act = 1 if sum(inc_acts) >= threshold else 0
+            threshold += self.epsilon
         self.nodes = new_nodes
         
     def show_pyvis(self, name="rbn"):
@@ -132,7 +140,57 @@ class RBN:
                 net.add_edge(i, self.edges[idx])        
             
         net.toggle_physics(True)
-        net.show(f"{name}.html", notebook=False)
+        net.show(f"{name}.html")
 
-rbn = RBN("./net/01.net")
-rbn.sync_update()
+    def run(self):
+        fig = plt.figure()
+        ax = Axes3D(fig)
+        cmap = ListedColormap(sns.color_palette("rocket", 2).as_hex())
+        fig.add_axes(ax)
+
+        def get_data():
+            x = [i.x for i in rbn.nodes]
+            y = [i.y for i in rbn.nodes]
+            z = [i.z for i in rbn.nodes]
+
+            return x,y,z
+        
+        x,y,z = get_data()
+
+        sc = ax.scatter(x,y,z,c=[i.act for i in rbn.nodes], cmap=cmap)
+        plt.legend(*sc.legend_elements(), bbox_to_anchor=(1.05, 1), loc=2)
+
+        def tick(_):
+            self.sync_update()
+
+            sc.set_facecolor([cmap(i.act) for i in rbn.nodes])
+
+        ani = matplotlib.animation.FuncAnimation(fig, tick, 
+                            interval=40, blit=False)
+
+        plt.show()
+
+# cmap = ListedColormap(sns.color_palette("rocket", 2).as_hex())
+# cmap(1)
+
+rbn = RBN("./net/01.net", 0.8)
+rbn.run()
+# rbn.show_pyvis()
+
+
+# sc.set_xdata
+
+# plt.show()
+
+# ax.scatter(
+
+
+# rbn.nodes[0]
+# sns.sc
+# # rbn.edges[:10]
+# # rbn.offsets[:10]
+
+# # next(pairwise(rbn.offsets, len(rbn.edges)))
+
+
+# # t = bitarray()
